8c09d29b5867174963804644b0cd3384cdb97702
Buyaki01 <rittahbuyaki@gmail.com>
2024-03-04 11:32:22 +0300

    Add fetch all users and fetch user query

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //fetch all users
+    $users = DB::select("select * from users");
+    dd($users);
+
+    //fetch a specific user
+    $user = DB::select("select * from users where email=?", ['rittahbuyaki@gmail.com']);
+    dd($user);
 });
 
 Route::get('/dashboard', function () {
