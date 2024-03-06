95410ceee8a7bb1df5c1761611bd98cd338a68d7
Buyaki01 <rittahbuyaki@gmail.com>
2024-03-06 14:07:34 +0300

    Use Storage Facade to delete old avatar whenever user updates their avatar

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request) {
         $path = $request->file('avatar')->store('avatars', 'public');
+
+        if ($oldAvatar = $request->user()->avatar) {
+            Storage::disk('public')->delete($oldAvatar);
+        }
         
         auth()->user()->update(['avatar' => $path]);
 
