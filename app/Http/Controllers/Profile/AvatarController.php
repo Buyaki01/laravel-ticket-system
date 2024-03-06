d0400877664d332b79fd49a70808cc0b234644ec
Buyaki01 <rittahbuyaki@gmail.com>
2024-03-06 09:24:02 +0300

    Add Form Request Validation

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -3,14 +3,11 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\UpdateAvatarRequest;
 
 class AvatarController extends Controller
 {
-    public function update(Request $request) {
-        $request->validate([
-            'avatar' => 'required|image'
-        ]);
+    public function update(UpdateAvatarRequest $request) {
         dd($request->all());
         return response()->redirectTo(route('profile.edit'))->with('success', 'Avatar was successfully updated.');
     }
